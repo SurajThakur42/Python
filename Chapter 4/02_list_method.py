f9b8ff0ce34d9176b5da6198b3f95ead5bbb6005
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:10:19 +0530

    Create 02_list_method.py

---
+++ Chapter 4/02_list_method.py
@@ -0,0 +1,21 @@
+datatype = ["Boolean","Integer","String","None"]
+
+print(datatype)
+
+datatype.append("Operator")
+print (datatype)
+
+datatype.sort()
+print (datatype)
+
+datatype.reverse()
+print (datatype)
+
+datatype.insert (0,None)
+print (datatype)
+
+datatype.pop(0)
+print (datatype)
+
+datatype.remove ('Integer')
+print(datatype)
