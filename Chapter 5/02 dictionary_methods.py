faa2ed2e628869a5326ef7a1ea4685da54d39370
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:15:28 +0530

    Create 02 dictionary_methods.py

---
+++ Chapter 5/02 dictionary_methods.py
@@ -0,0 +1,17 @@
+a = {
+     "Harry" : 78,
+    "Shubham" : 89,
+    "Sachin" : 94,
+    
+}
+
+
+print (a["Shubham"])
+
+print (a.items())
+print (a.keys())
+print (a.values())
+a.update({"Harry" : 89})
+print (a)
+
+print (a.get("Harry")) 
