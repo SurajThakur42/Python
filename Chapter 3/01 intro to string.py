9374d1ec29a3fa8bbcbd744287e7b0abce0270d6
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:04:05 +0530

    Create 01 intro to string.py

---
+++ Chapter 3/01 intro to string.py
@@ -0,0 +1,14 @@
+b = 'Suraj' #(Single quoted string)
+a = "Suraj" #(Double quoted string)
+c ="""Suraj""" #(Triple quoted string)
+
+name = "Suraj"
+print (len(name))
+character = name[0]
+print(character)
+nickname = name[0:5]
+print (nickname)
+
+
+
+
