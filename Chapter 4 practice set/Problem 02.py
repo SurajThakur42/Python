1438665aad4c6249cbd8a3da22bd027b7cb94a5b
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:12:47 +0530

    Create Problem 02.py

---
+++ Chapter 4 practice set/Problem 02.py
@@ -0,0 +1,14 @@
+a = int(input ("Enter your marks here :"))
+b = int(input ("Enter your marks here :"))
+c = int(input ("Enter your marks here :"))
+d = int(input ("Enter your marks here :"))
+e = int(input ("Enter your marks here :"))
+f = int(input ("Enter your marks here :"))
+
+Marks = [a,b,c,d,e,f]
+
+
+Marks.sort()
+print (Marks)
+
+
