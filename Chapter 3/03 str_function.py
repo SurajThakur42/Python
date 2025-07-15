5b5b092ad143e71e3ed88b8db24ea60a3c83dec7
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:05:11 +0530

    Create 03 str_function.py

---
+++ Chapter 3/03 str_function.py
@@ -0,0 +1,19 @@
+name = "Suraj"
+print (len(name ))
+print (name.endswith("raj"))
+print (name.startswith("asu"))
+print (name.startswith("sur"))
+print (name.startswith("Sur"))
+print (name.capitalize())
+print (name.title())
+print(name.upper())
+print (name.lower())
+print (name.strip())
+
+
+a = "Suraj is a coder."
+print (a.find("coder"))
+print (a.replace("coder",("player")))
+
+
+
