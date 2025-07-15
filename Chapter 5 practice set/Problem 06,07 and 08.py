05d362cc59117dc222ba7b8390712bc8795d1310
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:21:18 +0530

    Create Problem 06,07 and 08.py

---
+++ Chapter 5 practice set/Problem 06,07 and 08.py
@@ -0,0 +1,19 @@
+a ={}
+
+name = input("Enter you Good name : ")
+lang = input("Enter your Language : ")
+a.update ({name :lang})
+
+name = input("Enter you Good name : ")
+lang = input("Enter your Language : ")
+a.update ({name :lang})
+
+name = input("Enter you Good name : ")
+lang = input("Enter your Language : ")
+a.update ({name :lang})
+
+name = input("Enter you Good name : ")
+lang = input("Enter your Language : ")
+a.update ({name :lang})
+
+print (a)
