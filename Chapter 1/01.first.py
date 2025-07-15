2c2bc5637cef1a06fb77abed8825feb0bc19f7f7
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 21:44:37 +0530

    Create 01.first.py

---
+++ Chapter 1/01.first.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print ("This is my first code and i am very exiceted")
+
+print ("In double code, interpreter runs and write same code without executing it.")
+print ("Without code interpreter search that variable or anything you write above before executing or run.")
+print ("I am here to begin my python programming language.")
+
