0794196cf2ecff8278b9cbad04a44d98baba1cec
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:19:06 +0530

    Create Problem 02.py

---
+++ Chapter 5 practice set/Problem 02.py
@@ -0,0 +1,11 @@
+a = int(input ("Enter digit First Digit:"))
+b = int(input ("Enter digit Second Digit:")) 
+c = int(input ("Enter digit Third Digit:")) 
+d = int(input ("Enter digit Fourth Digit:"))
+e = int(input ("Enter digit Fifth Digit:"))
+f = int(input ("Enter digit Sixth Digit:"))
+g = int(input ("Enter digit Seventh Digit:"))
+h = int(input ("Enter digit Eight Digit:"))
+
+S = {a,b,c,d,e,f,g,h,}
+print (S)
