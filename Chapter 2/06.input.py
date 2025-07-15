8204735fa178af255dd2b816f724efeaffb78319
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 21:59:19 +0530

    Create 06.input.py

---
+++ Chapter 2/06.input.py
@@ -0,0 +1,27 @@
+a = float(input("Enter number 1: "))
+b = float(input("Enter number 2: "))
+
+print ("Number a is :", a )
+print ("Number b is :", b )
+print ("Addition is" ,int ( a + b ))
+
+
+
+
+
+a = input("Enter name 1 is :")
+b = input("Enter name 2 is :")
+
+print ("Full name is :", a+b)
+
+
+
+
+
+
+p = input ("Enter number 1 :" )
+q = input ("Enter number 2 :" )
+print("p is ", p)
+print ("q is", q)
+
+print ("Division is :", a/b)
