d7fcf96e99a07ae1d265fabf1cbc244db6e5232d
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 21:58:46 +0530

    Create 05.type.py

---
+++ Chapter 2/05.type.py
@@ -0,0 +1,29 @@
+a = 54 # <class 'int'>
+t = type(a)
+print (t)
+
+
+b = True # <class 'int'>
+t = type (b)
+print (t)
+
+
+c = None 
+t = type (c)
+print (t)
+
+
+x = 57
+y = float(x) # y should be floating type of x/='[]
+print (y)
+
+
+a = "56"
+b = float(a)
+t = type (a)
+print (b)
+
+
+p = 65
+q = str(p)
+print (q)
