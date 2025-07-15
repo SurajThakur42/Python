4a080e008c2a4af91a2efa43da10243f4a6b2e7e
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 21:58:06 +0530

    Create 04.Operators.py

---
+++ Chapter 2/04.Operators.py
@@ -0,0 +1,92 @@
+# 1). Arithmetic operators
+a = 3 
+b = 6
+c = a+b #(Addition)
+
+print (c)
+
+d = a-b #(Subtraction)
+print(d)
+
+e = a*b #(Multiplication)
+print (e)
+
+f = c/d #(Division)
+print(f)
+
+# 2). Assingment operators
+
+a = 42 # Assign 42 in a 
+b = 64 # Assign 64 in b
+print (b)
+
+b += 36 #(Increase value of b by 36 and then assign to b)
+
+print (b)
+
+c = 94
+c -= 4 #(Decrease value of c by 4 and assign it to c)
+print(c)
+
+d = 1
+d *= 7 #(Multiply value of d by 7 and assign to d)
+print (d)
+
+e = 49 
+e /= 7 #(Divide value of f by 7 and assign to e)
+print (e)
+
+# 3). Comparison operators
+
+g = 4<5
+print (g)
+
+
+f = 5==4
+print (f)
+
+h = 4!=7
+print (h)
+
+i = 5>=4
+print (i)
+
+
+# 4). Logical operator
+j = True or False
+print (j)
+
+
+# Truth table of "or"
+print ("True or False is True")
+print ("True or True is True")
+print ("False or False is False")
+print ("False or True is True")
+
+
+# Truth table of "and"
+print ("True and True is True")
+print ("False and False is False")
+print ("True and False is False")
+print ("False and True is False")
+
+# Not Operators 
+print (not(True))
+print(not(False))
+
+
+k = True and False
+print (k)
+
+l = (not(False))
+print(l)
+
+m = 5 > 6
+print (m)
+
+n = 57
+n += 3
+print (n)
+
+o = None 
+print (o)
