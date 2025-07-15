c1b1c8e5d9752ae083819b12ec7cafde740b79f0
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 22:12:23 +0530

    Create Problem 01.py

---
+++ Chapter 4 practice set/Problem 01.py
@@ -0,0 +1,10 @@
+a = input("Enter First fruit you want to store in list :")
+b = input("Enter Second fruit you want to store in list :")
+c = input("Enter Third fruit you want to store in list :")
+d = input("Enter Fourth fruit you want to store in list :")
+e = input("Enter Fifth fruit you want to store in list :")
+f = input("Enter Sixth fruit you want to store in list :")
+g = input("Enter Seventh fruit you want to store in list :")
+
+Fruit = [a,b,c,d,e,f,g]
+print (Fruit)
