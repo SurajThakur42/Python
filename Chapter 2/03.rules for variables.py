0214fd8f2a4e3fc59b22a98f2a04e5012ea73dcf
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-15 21:57:15 +0530

    Create 03.rules for variables.py

---
+++ Chapter 2/03.rules for variables.py
@@ -0,0 +1,9 @@
+a = 45
+aaaaaa = 65
+sameer = 85
+_sameer = 45
+
+
+# 23rahul = 65  (Invalid due to starting with number)
+# @rahul = 74     (Invalid due to starting with @)
+# Ra hu = True      (Invalid due to space between name of variable)
