1bf5d584db5a3be61ba24c01fb09417c96c86fe3
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:21:12 +0530

    Create 06_Problem 6.py

---
+++ Chapter 6 practice set/06_Problem 6.py
@@ -0,0 +1,7 @@
+post = (input("Enter your post :"))
+
+if ("Harry".capitalize()  in post.capitalize()):
+    print ("This post is talking about Harry.")
+
+else:
+    print("This post is not talking about Harry.")
