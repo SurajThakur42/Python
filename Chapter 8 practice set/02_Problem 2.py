7b486425bc489a9e18363652591fba484eadd88c
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:38:13 +0530

    Create 02_Problem 2.py

---
+++ Chapter 8 practice set/02_Problem 2.py
@@ -0,0 +1,7 @@
+def temperature():
+    a = int(input("Enter degree celsius temperature ,you want to convert in fahrenheit. : "))
+    print (a+273.15)
+
+temperature ()
+temperature ()
+temperature ()
