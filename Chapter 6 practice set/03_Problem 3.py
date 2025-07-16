c989a5eb598a68652c032954626c97dda37ea08f
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:19:41 +0530

    Create 03_Problem 3.py

---
+++ Chapter 6 practice set/03_Problem 3.py
@@ -0,0 +1,10 @@
+username = input("Enter your name : ")
+if(len(username) < 10) :
+    print ("Yes, Your name contain less than 10 characters.")
+
+elif(len(username) == 10) :
+    print ("Your name contain 10 characters.")
+
+else :
+    print ("Your name does not contain 10 characters.")
+
