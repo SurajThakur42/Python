cd8a2b5020c94a11751582106346981170d5f446
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 13:38:58 +0530

    Create main.py

---
+++ Project 1 = Snake,Water and Gun/main.py
@@ -0,0 +1,38 @@
+import random
+
+computerno= random.choice(["snake","water","gun"])
+yourchoiceno  = input("Enter your choice between snake,water and gun :")
+computer = computerno.lower()
+yourchoice = yourchoiceno.lower()
+
+print (f"You choose {yourchoice} and  Computer choose {computer}")
+if (computer == yourchoice):
+    print("This is a draw")
+
+else:
+
+    if(computer == "snake" and yourchoice == "water"):
+        print ("Computer Win!")
+
+    elif(computer == "snake" and yourchoice == "gun"):
+        print ("You Win!")
+
+    elif(computer == "water" and yourchoice == "snake"):
+        print ("You Win!")
+
+    elif(computer == "water" and yourchoice == "gun"):
+        print ("Computer Win!")
+
+    elif(computer == "gun" and yourchoice == "snake"):
+        print ("Computer Win!")
+
+    elif(computer == "gun" and yourchoice == "water"):
+        print ("You Win!")
+
+
+    else :
+        print ("Sorry,Something went wrong.")
+
+
+    
+
