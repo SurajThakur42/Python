794fa9bbec41b8dcd29e7fd960598b4be650a8ef
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:20:10 +0530

    Create 04_Problem 4.py

---
+++ Chapter 6 practice set/04_Problem 4.py
@@ -0,0 +1,9 @@
+list = {"Suraj,Shubham,Rohan,Prakash,Rahul"}
+
+Name = input("Enter given name : ")
+
+if(Name == list):
+    print ("Given name is present in list.")
+
+else:
+    print ("Given name is not in list.")
