a3b1516862f4a1997ed5484a9250e7ab68a3b544
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:17:07 +0530

    Create 03_multiple if.py

---
+++ Chapter 6/03_multiple if.py
@@ -0,0 +1,27 @@
+a = int(input("enter your age"))
+
+# if statement no . 1
+
+if(a%2 == 0) :
+    print ("a is divisible by 2")
+
+
+# if statement no . 2
+
+if(a==18>100):
+    print ("you are above the age of concent")
+    print ("Good for you")
+
+elif(a<0):
+    print ("You are entering invalid age")
+
+elif(a ==0):
+    print ("you are just kiding")
+
+elif(a>100):
+    print ("wow", "you are very strong and healthy person")
+
+else :
+    print ("you are minor now")   
+
+
