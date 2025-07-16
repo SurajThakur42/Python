ea17fff877eaabb6a25c11888aea14ec23a04e12
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:24:38 +0530

    Create 06_Break,continue and pass.py

---
+++ Chapter 7/06_Break,continue and pass.py
@@ -0,0 +1,21 @@
+for i in range (11):
+    if (i == 7):
+        break #Exit the loop
+    print (i)
+
+
+for i in range (19):
+    if (i == 14):
+        continue # skip this iteration (value)
+    print (i)
+
+
+for i in range(670):
+    pass #it instruct to do nothing.
+
+
+i = 0
+while(i<45):
+    print (i)
+    i+=1
+
