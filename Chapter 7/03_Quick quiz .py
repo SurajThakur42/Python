633ddf7f9df0125efbac94b962ec6199abde250b
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:23:11 +0530

    Create 03_Quick quiz .py

---
+++ Chapter 7/03_Quick quiz .py
@@ -0,0 +1,26 @@
+a = 1
+
+while (a<=50):
+    print (a)
+    a+=1
+
+i = 0 
+while (i <=5 ):
+    print ("Harry")
+    i += 1
+
+
+list = [1, 2,True,"Harry ", False]
+i = 0
+
+while (i<len(list)):
+    print (list[i])
+    i +=1
+
+
+l = [1,3,4,6,7,8,9]
+i = 0
+
+while (i<len(l)):
+    print (l[i])
+    i+=1
