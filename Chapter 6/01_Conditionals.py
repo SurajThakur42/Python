af09099acc2077de9a4a354cbb607be57ad4ca65
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:15:52 +0530

    Create 01_Conditionals.py

---
+++ Chapter 6/01_Conditionals.py
@@ -0,0 +1,23 @@
+a = int(input("Enter your age :"))
+
+if(a>=18>100):
+    print ("you are above the age of concent")
+    print ("Good for you")
+
+elif(a<0):
+    print ("You are entering invalid age")
+
+elif(a ==0):
+    print ("you are just kiding")
+
+elif(a>100):
+    print ("wow", "you are very strong and healthy person")
+
+else :
+    print ("you are minor now")   
+
+a = (input("Enter what do you want to make: "))
+
+if ("a = website"):
+    print ("Great")
+
