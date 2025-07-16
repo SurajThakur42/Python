cb4fcbef75241ffb1cb06f6bd491950b5a6c6065
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:34:05 +0530

    Create 01_functions.py

---
+++ Chapter 8/01_functions.py
@@ -0,0 +1,36 @@
+# a = 12
+# b =45
+# c = 67
+
+# avg = (a+b+c)/3
+# print (avg)
+
+# a = 12
+# b =45
+# c = 67
+
+# avg = (a+b+c)/3
+# print (avg)
+
+# a = 12
+# b =45
+# c = 67
+
+# avg = (a+b+c)/3
+# print (avg)
+
+
+
+def avg():  # Definiton of function
+    a = int(input("Enter the number : "))
+    b = int(input("Enter the number : "))
+    c = int(input("Enter the number : "))
+
+    avg = (a+b+c)/3
+    print (avg)
+    
+
+a = avg()   # Function call
+print ("Thanks")
+avg()
+print ("Thanks")
