10f1f99f1e7f13f80c182a8a03c8fa0a9ba9f881
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-16 12:18:24 +0530

    Create 01_Problem 1.py

---
+++ Chapter 6 practice set/01_Problem 1.py
@@ -0,0 +1,16 @@
+a = int(input("Enter first digit : "))
+b = int (input("Enter second digit : "))
+c = int (input("Enter Third digit : "))
+d = int (input("Enter Fourth digit : "))
+
+if(a>b>c>d):
+    print ("a)", a)
+
+if (a<b>c>d):
+    print ("b)", b)
+
+if(a<b<c>d):
+    print ("c)", c)
+
+if(a<b<c<d):
+    print ("d)", d)
